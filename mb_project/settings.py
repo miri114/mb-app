37acb4e4bfc8866e94a6625418413866da8421ed
Miri <delomiremini@gmail.com>
2020-12-30 13:54:44 +0100

    New updates for Heroku deployment

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's@%l&p@j7%-01ut%xzx6w%qo2dpd65l_6=r=m+sy)jchh(=ei='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
